01dd53c7d0e380259cb1fcfd07361ad8e6ec5049
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2024-08-11 09:04:59 +0500

    worked with statefull widgets

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -1,34 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main(){
-
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
 
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My first App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const  Center(
-            child:  Text('Welcome Back'),
-          ),
-          backgroundColor: Colors.grey,
-        ),
-        body: const Center(
-          child: Text('Zulfiqar Alam\n Hello world')
-
-        ),
-
-      )
+      title: 'Interactive App',
+      home: MyHomePage(),
     );
   }
+}
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
+  String _displayText = "Hello, World!";
+
+  void _changeText() {
+    setState(() {
+      _displayText = "You pressed the button!";
+    });
   }
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Welcome to My App'),
+        ),
+        backgroundColor: Colors.grey,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _displayText,
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _changeText,
+              child: Text('Press Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
