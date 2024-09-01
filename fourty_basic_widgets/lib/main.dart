41316d7244b0fca1b5393d172777f4ccfec900a4
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2024-09-01 18:06:09 +0500

    stack widget

--- fourty_basic_widgets/lib/main.dart
+++ fourty_basic_widgets/lib/main.dart
@@ -1,92 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const BasicWidgets());
+  runApp(MyApp());
 }
 
-class BasicWidgets extends StatelessWidget {
-  const BasicWidgets({super.key});
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Basic Widgets',
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Basic Widgets'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-        //  mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 20),
-            Container(
-             padding: EdgeInsets.all(24.0),
-              margin: EdgeInsets.all(24.0),
-              width: 200,
-              height: 200,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.teal,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(1),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0,3),
-                  )
-                ]
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stack Widget Example'),
+        ),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center, // Aligns children in the center of the Stack
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.blue,
               ),
-              child: Center(child: Text('Container',style: TextStyle(fontSize: 24,color: Colors.white),)),
-            ),
-        Container(
-          width: 200,
-          height: 200,
-          padding: EdgeInsets.all(30.0),
-          margin: EdgeInsets.all(50.0),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3), // changes position of shadow
+              Positioned(
+                top: 50,
+                left: 50,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+              ),
+              Positioned(
+                bottom: 75,
+                left: 75,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.green,
+                ),
+              ),
+              Text(
+                'Stacked Text',
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
             ],
           ),
-          child: Text(
-            'Hello, Container!',
-            style: TextStyle(color: Colors.white, fontSize: 18),
-          ),
-        ),
-
-
-
-
-
-
-
-    ],
         ),
       ),
     );
